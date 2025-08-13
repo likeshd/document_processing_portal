3c7d1f64dbc40f894440116eb4e13671ea96b4d0
LikeshD <likesh.dahake@assettl.com>
2025-08-13 10:40:38 +0530

    optimized code

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="document_processing_portal",
     author="Likesh Dahake",
-    version="1.0",
+    version="0.1",
     packages=find_packages(),
 )
