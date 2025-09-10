4aee1164935cf3d8a156830288db8e17c2f85d42
LikeshD <likesh.dahake@assettl.com>
2025-09-10 10:07:52 +0530

    added test case

--- tests/test_unit_cases.py
+++ tests/test_unit_cases.py
@@ -9,3 +9,9 @@ def test_home():
     response = client.get("/")
     assert response.status_code == 200
     assert "Document Portal" in response.text
+
+# def test_upload_file():
+#     with open("tests/sample.pdf", "rb") as f:
+#         response = client.post("/upload", files={"file": ("sample.pdf", f, "application/pdf")})
+#     assert response.status_code == 200
+#     assert response.json()["filename"] == "sample.pdf"
